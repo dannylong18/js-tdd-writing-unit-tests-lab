be6c3da5742d2d3fcaade280c79988dbee950ba0
Daniel Long <danielwlong618@gmail.com>
2024-01-06 13:15:50 -0600

    lab done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,34 @@
 // Your tests here
+import { isPalindrome } from '../utils'
+
+describe ('isPalindrome', () => {
+    it ('returns true if word is a palindrome', () => {
+        const word = 'mom';
+
+        const value = isPalindrome(word);
+
+        expect(value).toBe(true)
+    })
+
+    it ('returns false if word is not a palindrome', () => {
+        const word = 'hello';
+
+        const value = isPalindrome(word);
+
+        expect(value).toBe(false)
+    })
+
+    it ('returns true for combination of uppercase and lowercase letters', () => {
+        const word = 'RaCEcaR'
+
+        const value = isPalindrome(word)
+
+        expect(value).toBe(true)
+    })
+
+    it ('throws an error if the word is an empty string', () => {
+        const word = '';
+
+        expect(() => isPalindrome(word)).toThrow()
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function isPalindrome (word) {
+    if (!/^[A-Za-z]+$/.test(word)) {
+        throw new Error("Invalid input");
+      }
+      
+    const regWord = word.toLowerCase()
+    const reverseWord = word.toLowerCase().split('').reverse().join('')
+
+    const value = regWord === reverseWord
+
+    return value
+}
